25fc796d7084a486a1668c2db6431fbce3787c7f
SantiagoArroyoL <santiagoarroyo@ciencias.unam.mx>
2020-06-07 21:25:14 -0500

    Última práctica de Canék completaada!

---
+++ Java/ESCUELA/Practicas_Canek/practica11/src/main/java/mx/unam/ciencias/edd/Conjunto.java
@@ -0,0 +1,157 @@
+package mx.unam.ciencias.edd;
+
+import java.util.Iterator;
+
+/**
+ * Clase para conjuntos.
+ */
+public class Conjunto<T> implements Coleccion<T> {
+
+    /* El conjunto de elementos. */
+    private Diccionario<T, T> conjunto;
+
+    /**
+     * Crea un nuevo conjunto.
+     */
+    public Conjunto() {
+        // Aquí va su código.
+		conjunto = new Diccionario<>();
+    }
+
+    /**
+     * Crea un nuevo conjunto para un número determinado de elementos.
+     * @param n el número tentativo de elementos.
+     */
+    public Conjunto(int n) {
+        // Aquí va su código.
+		conjunto = new Diccionario<>(n);
+    }
+
+    /**
+     * Agrega un elemento al conjunto.
+     * @param elemento el elemento que queremos agregar al conjunto.
+     * @throws IllegalArgumentException si el elemento es <code>null</code>.
+     */
+    @Override public void agrega(T elemento) {
+        // Aquí va su código.
+		conjunto.agrega(elemento,elemento);
+    }
+
+    /**
+     * Nos dice si el elemento está en el conjunto.
+     * @param elemento el elemento que queremos saber si está en el conjunto.
+     * @return <code>true</code> si el elemento está en el conjunto,
+     *         <code>false</code> en otro caso.
+     */
+    @Override public boolean contiene(T elemento) {
+        // Aquí va su código.
+		return conjunto.contiene(elemento);
+    }
+
+    /**
+     * Elimina el elemento del conjunto, si está.
+     * @param elemento el elemento que queremos eliminar del conjunto.
+     */
+    @Override public void elimina(T elemento) {
+        // Aquí va su código.
+		if (elemento != null)
+			conjunto.elimina(elemento);
+    }
+
+    /**
+     * Nos dice si el conjunto es vacío.
+     * @return <code>true</code> si el conjunto es vacío, <code>false</code> en
+     *         otro caso.
+     */
+    @Override public boolean esVacia() {
+        // Aquí va su código.
+		return conjunto.esVacia();
+    }
+
+    /**
+     * Regresa el número de elementos en el conjunto.
+     * @return el número de elementos en el conjunto.
+     */
+    @Override public int getElementos() {
+        // Aquí va su código.
+		return conjunto.getElementos();
+    }
+
+    /**
+     * Limpia el conjunto de elementos, dejándolo vacío.
+     */
+    @Override public void limpia() {
+        // Aquí va su código.
+		conjunto.limpia();
+    }
+
+    /**
+     * Regresa la intersección del conjunto y el conjunto recibido.
+     * @param conjunto el conjunto que queremos intersectar con éste.
+     * @return la intersección del conjunto y el conjunto recibido.
+     */
+    public Conjunto<T> interseccion(Conjunto<T> conjunto) {
+        // Aquí va su código.
+		Conjunto<T> s = new Conjunto<>();
+		for (T elemento : conjunto)
+			if (contiene(elemento))
+				s.agrega(elemento);
+		return s;
+    }
+
+    /**
+     * Regresa la unión del conjunto y el conjunto recibido.
+     * @param conjunto el conjunto que queremos unir con éste.
+     * @return la unión del conjunto y el conjunto recibido.
+     */
+    public Conjunto<T> union(Conjunto<T> conjunto) {
+        // Aquí va su código.
+		Conjunto<T> s = conjunto;
+		for (T elemento : this.conjunto)
+			s.agrega(elemento);
+		return s;
+    }
+
+    /**
+     * Regresa una representación en cadena del conjunto.
+     * @return una representación en cadena del conjunto.
+     */
+    @Override public String toString() {
+        // Aquí va su código.
+		if (conjunto.esVacia())
+			return "{}";
+		int i = 1;
+		String sFinal = "{ ";
+		for (T elemento : conjunto) {
+			if (i == 1)
+				sFinal += elemento.toString();
+			else
+				sFinal += ", " + elemento.toString();
+			i++;
+		}
+		return sFinal + " }";
+    }
+
+    /**
+     * Nos dice si el conjunto es igual al objeto recibido.
+     * @param o el objeto que queremos saber si es igual al conjunto.
+     * @return <code>true</code> si el objeto recibido es instancia de Conjunto,
+     *         y tiene los mismos elementos.
+     */
+    @Override public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass())
+            return false;
+        @SuppressWarnings("unchecked") Conjunto<T> c = (Conjunto<T>)o;
+        // Aquí va su código.
+		return conjunto.equals(c.conjunto);
+    }
+
+    /**
+     * Regresa un iterador para iterar el conjunto.
+     * @return un iterador para iterar el conjunto.
+     */
+    @Override public Iterator<T> iterator() {
+        // Aquí va su código.
+		return conjunto.iterator();
+    }
+}
